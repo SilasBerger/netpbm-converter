e6a5ebc6cfa96b9b374c946174eec2c7f0c0874a
Silas Berger <silas.berger@outlook.com>
2024-02-25 12:12:50 +0100

    Implement P1 and P2.

--- convert.py
+++ convert.py
@@ -26,15 +26,42 @@ def extract_args():
     return args[1].upper(), pathlib.Path(args[2])
 
 
+def write_to_file(lines: [[str]], out_dir: pathlib.Path, base_filename: str, suffix: str):
+    out_path = out_dir / f"{base_filename}.{suffix}"
+    with open(out_path, 'w') as outfile:
+        for line in lines:
+            outfile.write(' '.join(line))
+            outfile.write('\n')
+
+
 def convert_p1(image: numpy.ndarray, out_dir: pathlib.Path, base_filename: str):
-    raise Exception('Not yet implemented.')
+    lines = [['P1']]
+
+    height, width, _ = image.shape
+    lines.append([str(width), str(height)])
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, black_and_white_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    for row in black_and_white_image:
+        lines.append(['1' if val == 0 else '0' for val in row])
+
+    write_to_file(lines, out_dir, base_filename, 'ppm')
 
 
-def convert_p2():
-    raise Exception('Not yet implemented.')
+def convert_p2(image: numpy.ndarray, out_dir: pathlib.Path, base_filename: str):
+    lines = [['P2']]
+
+    height, width, _ = image.shape
+    lines.append([str(width), str(height)])
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    for row in gray_image:
+        lines.append([str(val) for val in row])
+
+    write_to_file(lines, out_dir, base_filename, 'pgm')
 
 
-def convert_p3():
+def convert_p3(image: numpy.ndarray, out_dir: pathlib.Path, base_filename: str):
     raise Exception('Not yet implemented.')
 
 
@@ -46,9 +73,9 @@ def convert(input_path: pathlib.Path, mode: str):
     if mode == P1:
         convert_p1(image, out_dir, base_filename)
     elif mode == P2:
-        convert_p2()
+        convert_p2(image, out_dir, base_filename)
     elif mode == P3:
-        convert_p3()
+        convert_p3(image, out_dir, base_filename)
     else:
         print(f"Invalid mode: {mode}\n")
         print_usage()
